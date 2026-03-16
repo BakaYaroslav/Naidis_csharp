716bf23f1f0c0442ad8bc64ed9b47b73e6109670
yaroslav <jaroslavbaka25@gmail.com>
2026-03-16 11:40:29 +0200

    osa 5 start

---
+++ Naidis_csharp/osa5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naidis_csharp
+{
+    internal class osa5
+    {
+        public static void Main(string[] args)
+        {
+            osa5_funktsioonid.array_naide();
+        }
+    }
+}
---
+++ Naidis_csharp/start_page_osa5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naidis_csharp
+{
+    internal class start_page_osa5
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("osa5 funktsioonid");
+            string valik = Console.ReadLine();
+            switch (valik)
+            {
+                case "1":
+                    osa5_funktsioonid.array_naide();
+                    break;
+                case "2":
+                    osa5_funktsioonid.Tuple();
+                    break;
+                case "3":
+                    Person.List_klassiga();
+                    break;
+                case "4":
+                    osa5_funktsioonid.LinkedList();
+                    break;
+                default:
+                    Console.WriteLine("Valik puudub");
+                    break;
+
+            }
+        }
+	}
+
+}
+
