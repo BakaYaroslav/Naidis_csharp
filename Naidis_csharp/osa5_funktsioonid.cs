92d6775154977c9b595b1e684aaa7b0005c89e4d
opilane <opilane@HP-CZC2349HTN>
2026-03-18 14:57:21 +0200

    commit esimene ül

--- Naidis_csharp/osa5_funktsioonid.cs
+++ Naidis_csharp/osa5_funktsioonid.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.Versioning;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
@@ -18,7 +21,7 @@ namespace Naidis_csharp
                 nimed.Add("Kati");
                 nimed.Add("Mati");
                 nimed.Add("Juku");
-               
+
 
                 if (nimed.Contains("Mati"))
                     Console.WriteLine("Mati olemas");
@@ -43,13 +46,15 @@ namespace Naidis_csharp
             Tuple<float, char> route = new Tuple<float, char>(2.5f, 'N');
             Console.WriteLine($"Vahemaa: {route.Item1}, Suund: {route.Item2}");
         }
+
+
         public static void LinkedList()
         {
             LinkedList<int> loetelu = new LinkedList<int>();
             loetelu.AddLast(5);
             loetelu.AddLast(3);
             loetelu.AddFirst(0);
-
+            Console.WriteLine("-----------------------------");
             foreach (int arv in loetelu)
                 Console.WriteLine(arv);
 
@@ -57,6 +62,13 @@ namespace Naidis_csharp
             loetelu.RemoveLast();
             loetelu.AddLast(555);
             loetelu.Remove(555);
+            LinkedListNode<int> a = loetelu.Find(5);
+            loetelu.AddBefore(a, 11);
+
+            loetelu.AddAfter(a, 22);
+            Console.WriteLine("-----------------------------");
+            foreach (int arv in loetelu)
+                Console.WriteLine(arv);
         }
         public static void sonatlik()
         {
@@ -65,12 +77,37 @@ namespace Naidis_csharp
             riigid.Add(2, "Eesti");
             riigid.Add(3, "Itaalia");
 
+            riigid.ContainsKey(1);
+            riigid.ContainsValue("Itaalia");
+
             foreach (var paar in riigid)
                 Console.WriteLine($"{paar.Key} - {paar.Value}");
 
             string pealinn = riigid[2];
             riigid[2] = "Eestimaa";
             riigid.Remove(3);
+
+        }
+
+
+
+
+    }
+
+
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public Person()
+        {
         }
 
         public static void sonatlikKlassiga()
@@ -82,24 +119,6 @@ namespace Naidis_csharp
             foreach (var entry in inimesed)
                 Console.WriteLine($"{entry.Key} - {entry.Value.Name}");
         }
-
-
-    }
-
-
-
-    internal class Person
-    {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-
-        public Person(string name, string surname)
-        {
-            Name = name;
-            Surname = surname;
-        }
-
-
         public static void List_klassiga()
         {
             List<Person> people = new List<Person>();
@@ -121,20 +140,27 @@ namespace Naidis_csharp
         public static void Insert_Listisse()
         {
             List<Person> people = new List<Person>();
-            people.Remove(new Person() { Name = "Kadi" });
-            people.Insert(1, "Juku");
+            people.Insert(1, new Person() { Name = "Kadi" });
+            people.Remove(new Person() { Name = "Mirje" });
             foreach (Person p in people)
                 Console.WriteLine(p.Name);
         }
-        public static void osting_findindex()
+        public static void kustuta_objekt()
         {
+
             List<Person> people = new List<Person>();
-            people.Add(new Person() { Name = "Kadi" });
-            people.Add(new Person() { Name = "Mirje" });
+
+            foreach (Person p in people)
+                Console.WriteLine(p.Name);
+            Person p1 = new Person() { Name = "Kadi" };
+            people.Add(p1);
+
             Console.WriteLine($"Person 1 index on: {people.FindIndex(p => p.Name == "Kadi")}");
-            people.Remove(Person)
+            people.Remove(p1);
+            foreach (Person p in people)
+                Console.WriteLine(p.Name);
         }
-        public static void remove_objekt()
+        public static void remove_objekt2()
         {
             List<Person> people = new List<Person>();
             people.Add(new Person() { Name = "Kadi" });
@@ -142,10 +168,176 @@ namespace Naidis_csharp
 
             people.Remove(new Person("Lisa"));
         }
+        public static void remove_lamda_abil()
+        {
+            List<Person> people = new List<Person>();
+            Person p1 = new Person() { Name = "Kadi" };
+            people.Add(p1);
+            int i = people.FindIndex(p => p.Name == "Kadi");
+            people.RemoveAt(i);
+            Console.WriteLine($"Person kustutanud on: {i}; {p1.Name}");
+        }
+
+        public static void list_sort_ja_remove()
+        {
+            List<string> people = new List<string>() { "Kadi", "Anna" };
+            people.Sort();
+
+            foreach (string p in people)
+                Console.WriteLine(p);
+            int nimi1 = people.BinarySearch("Kadi");
+            Console.WriteLine(nimi1);
+
+            Console.WriteLine("Sisesta nimi: ");
+            string nimi = Console.ReadLine();
+            people.Remove(nimi);
+            foreach (string p in people)
+                Console.WriteLine(p);
+
+            //people.Sort((a, b) => a.Length.CompareTo(b.Length));
+            //foreach (string p in people)
+            //    Console.WriteLine(p);
+        }
+
+
+
         
 
+
     }
+}
+
+
+
+
+
+
+
+
+
+    // OSA 5 ÜLESANDED
+
+    public class Toode
+    {
+        static string Nimi { get; set; }
+        static string Kalorid100g { get; set; }
+
+        public Toode(string name, string kalorid100g)
+        {
+            Nimi = name;
+            Kalorid100g = kalorid100g;
+        }
+
+
+
+    }
+        public class Inimene
+        {
+            static string Nimi { get; set; }
+            static int Vanus { get; set; }
+            static string Sugu { get; set; }
+            static double Pikkus { get; set; }
+            static double Kaal { get; set; }
+            static double Aktiivsustase { get; set; }
+
+                public Inimene(string nimi, int vanus, string sugu, double pikkus, double kaal, double aktiivsustase)
+                {
+                    Nimi = nimi;
+                    Vanus = vanus;
+                    Sugu = sugu;
+                    Pikkus = pikkus;
+                    Kaal = kaal;
+                    Aktiivsustase = aktiivsustase;
+                }
+                  
 
 }
+
+
+
+public class ulesanded
+{
+    public static void Toidu_salvestamine_faili()
+    {
+        try
+        {
+            string retseptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Tooded.txt");
+
+
+
+            if (File.Exists(retseptPath))
+            {
+                Console.WriteLine("Olemasolevad toidud");
+                foreach (string rida in File.ReadAllLines(retseptPath))
+                    Console.WriteLine($"  - {rida}");
+                Console.WriteLine();
+            }
+
+
+
+
+
+           
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.Write("Sisesta uus toidu nimi: ");
+                string toit = Console.ReadLine();
+                using (StreamWriter retseptWriter = new StreamWriter(retseptPath, true))
+                {
+                        Console.Write("sisesta kaal:  ");
+                        string kaal = Console.ReadLine();
+             
+                        retseptWriter.Write($"{toit};{kaal}");
+                        retseptWriter.Close();
+                }
+
+
+                if (!string.IsNullOrWhiteSpace(toit))
+                {
+
+                    Console.WriteLine($"{toit} lisatud!");
+                }
+                key = Console.ReadKey(true);
+            } while (key.Key != ConsoleKey.Backspace);
+
+
+            Console.WriteLine(" Kõik salvestatud!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Viga: {e.Message}");
+        }
+    }
+
+    public static void KaalKalkulaator()
+    {
+        List<string> list = new List<string>();
+        try
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Tooded.txt");
+            foreach (string rida in File.ReadAllLines(path))
+                list.Add(rida);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Viga faili lugemisel!");
+        }
+
+        foreach (string rida in list)
+            Console.WriteLine(rida);
+
+        double kaal = 0;
+        if ()
+        {
+
+        }
+
+    }
+}
+    
+
+
+
 
 
--- Naidis_csharp/start_page_osa5.cs
+++ Naidis_csharp/start_page_osa5.cs
@@ -11,9 +11,11 @@ namespace Naidis_csharp
         public static void Main(string[] args)
         {
             System.Console.WriteLine("osa5 funktsioonid");
+         
             string valik = Console.ReadLine();
             switch (valik)
             {
+
                 case "1":
                     osa5_funktsioonid.array_naide();
                     break;
@@ -25,6 +27,31 @@ namespace Naidis_csharp
                     break;
                 case "4":
                     osa5_funktsioonid.LinkedList();
+                    break;
+                case "5":
+                    osa5_funktsioonid.sonatlik();
+                    break;
+                case "6":
+                    ulesanded.Toidu_salvestamine_faili();
+                    break;
+                case "7":
+                    Console.Write("Sisesta oma nimi: ");
+                    string nimi = Console.ReadLine();
+
+                    Console.Write("Sisesta vanus: ");
+                    int vanus = int.Parse(Console.ReadLine());
+
+                    Console.Write("Sisesta sugu (mees/naine): ");
+                    string sugu = Console.ReadLine().ToLower();
+
+                    Console.Write("Sisesta pikkus (cm): ");
+                    double pikkus = double.Parse(Console.ReadLine());
+
+                    Console.Write("Sisesta kaal (kg): ");
+                    double kaal = double.Parse(Console.ReadLine());
+
+                    Inimene kasutaja = new Inimene(nimi, vanus, sugu, pikkus, kaal, aktiivsustase);
+                    ulesanded.KaalKalkulaator(kasutaja);
                     break;
                 default:
                     Console.WriteLine("Valik puudub");
